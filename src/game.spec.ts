c653c2b987a0fcb5fa447c4755fcadd00d6ad569
Matthew Briggs <matthew.briggs@cazoo.co.uk>
2022-04-01 15:34:20 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,22 +1,66 @@
-class Game {
-    current = (): { title: string, description: string } => {
-
-        return {
-            description: "YOU ARE STANDING AT THE END OF BRICK LANE BEFORE A SMALL BRICK BUILDING CALLED THE OLD TRUMAN BREWERY. \n" +
-                "AROUND YOU IS A FOREST OF INDIAN RESTAURANTS. \n" +
-                "A SMALL STREAM OF CRAFTED BEER FLOWS OUT OF THE BUILDING AND DOWN A GULLY.",
-            title:'LOST IN SHOREDITCH.'
-        }
-    };
+class Room {
+  constructor(public title: string, public description: string) {}
 }
 
-describe('Game', function () {
-    it('should be initialised to the starting location', function () {
-        const game = new Game();
-        const location = game.current();
-        expect(location.title).toBe('LOST IN SHOREDITCH.')
-        expect(location.description).toBe('YOU ARE STANDING AT THE END OF BRICK LANE BEFORE A SMALL BRICK BUILDING CALLED THE OLD TRUMAN BREWERY. \n' +
-            'AROUND YOU IS A FOREST OF INDIAN RESTAURANTS. \n' +
-            'A SMALL STREAM OF CRAFTED BEER FLOWS OUT OF THE BUILDING AND DOWN A GULLY.')
-    });
+class Game {
+  constructor(private currentRoom: Room) {}
+
+  current = (): Room => {
+    return this.currentRoom;
+  };
+}
+
+describe("Game", function () {
+  it("should be initialised to the starting room", function () {
+    const initialRoom = new Room(
+      "LOST IN SHOREDITCH.",
+      "YOU ARE STANDING AT THE END OF BRICK LANE BEFORE A SMALL BRICK BUILDING CALLED THE OLD TRUMAN BREWERY. \n" +
+        "AROUND YOU IS A FOREST OF INDIAN RESTAURANTS. \n" +
+        "A SMALL STREAM OF CRAFTED BEER FLOWS OUT OF THE BUILDING AND DOWN A GULLY."
+    );
+
+    const game = new Game(initialRoom);
+    const room = game.current();
+
+    expect(room.title).toBe("LOST IN SHOREDITCH.");
+    expect(room.description).toBe(
+      "YOU ARE STANDING AT THE END OF BRICK LANE BEFORE A SMALL BRICK BUILDING CALLED THE OLD TRUMAN BREWERY. \n" +
+        "AROUND YOU IS A FOREST OF INDIAN RESTAURANTS. \n" +
+        "A SMALL STREAM OF CRAFTED BEER FLOWS OUT OF THE BUILDING AND DOWN A GULLY."
+    );
+  });
+
+  it("should move to a new room", () => {
+    const initialRoom = new Room(
+      "LOST IN SHOREDITCH.",
+      "YOU ARE STANDING AT THE END OF BRICK LANE BEFORE A SMALL BRICK BUILDING CALLED THE OLD TRUMAN BREWERY. \n" +
+        "AROUND YOU IS A FOREST OF INDIAN RESTAURANTS. \n" +
+        "A SMALL STREAM OF CRAFTED BEER FLOWS OUT OF THE BUILDING AND DOWN A GULLY."
+    );
+
+    const nextRoom = new Room("another room", "its empty");
+
+    initialRoom.addConnection("L", nextRoom);
+
+    const game = new Game(initialRoom);
+
+    const currentRoom = game.move("L");
+
+    expect(currentRoom).toBe(nextRoom);
+  });
 });
+
+/**\
+ 
+const startingRoom = new Room(blah blah)
+const nextRoom = new Room(other room)
+
+startingRoom.addConnection("L", nextRoom)
+nextRoom.addConnection("R", startingRoom)
+ 
+const game = new Game(startingRoom)
+
+game.move("L")
+
+ 
+ **/
